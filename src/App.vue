5dc1125c0ececf0d64e72b0b387190e9e0083d5e
IrynaVelykholova <irynavely93@gmail.com>
2023-08-24 04:09:18 +0200

    lavoro finito (se aggiungo TheFooter, si sovrappone al main) da correggere

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,86 @@
 <script>
-  import TheHeader from "./components/TheHeader.vue"
-  import TheFooter from "./components/TheFooter.vue"
+  import TheHeader from "./components/TheHeader.vue";
+  import dcComicsArray from "./dcComics.js";
+  import comicsCards from "./components/comicsCards.vue";
+  import TheFooter from "./components/TheFooter.vue";
+
 
   export default {
     components:{
       TheHeader,
+      comicsCards,
       TheFooter,
-    }
-} 
+    },
+    data() {
+      return {
+        dcComicsArray,
+      };
+  },
+}
+    
 </script>
 
 <template> 
   <TheHeader></TheHeader>
-  <main> Content goes here</main>
-  <ThePreFooter></ThePreFooter>
-  <TheFooter></TheFooter>
-</template>
-<style>  </style>
 
+  <main> 
+    <div class=" black-main">
+      <img class="img-jumbo" src="./assets/img/jumbotron.jpg" alt="">
+
+      <div class="text-center position-relative">
+        <button class="position-absolute button-blue button-absolute">CURRENT SERIES</button> 
+
+        <div class="row row-cols-6 gy-3 my-5 card-container">
+
+          <!-- ciclo for per le card -->
+          <div class="card bg-black" 
+            v-for="card in dcComicsArray">
+            
+            <comicsCards
+              :imgSrc="card.thumb"
+              :series="card.series" 
+              :type="card.type"
+              :price="card.price"
+            >
+            </comicsCards>
+          </div>
+
+          <button class="button-blue button-load">LOAD MORE</button>
+        </div>
+      </div>
+    </div>
+  </main>
+
+
+</template>
+
+<style lang="scss" scoped>
+@use "./styles/partials/variables" as *;
+
+.black-main {
+  background-color: black;
+  .img-jumbo {
+      height: 450px;
+      width: 100%;
+      object-position: top;
+      object-fit: cover;
+    }
+  .button-absolute {
+    top: -12%;
+    left: 15%;
+  }
+  .card-container {
+      max-width: 1280px;
+      margin: auto;
+    }
+  .button-blue {
+    background-color: $color-primary;
+    color: white;
+    padding: 1rem 2rem;
+    border: none;
+  }
+  .button-load {
+    margin: auto;
+  }
+}
+</style>
---
+++ src/components/comicsCards.vue
@@ -0,0 +1,56 @@
+<script>
+    export default {
+
+        props: {
+            imgSrc: String,
+            series: String,
+            type: String,
+            price: Number,
+        }
+    }
+</script>
+
+<template>
+        <div class="card border-0 h-100"> 
+            <img class="position-relative cards-img" :src="imgSrc" alt="" />
+
+            <div class="card-body text-white bg-black">
+                {{ series }}
+            </div>
+
+            <div>
+                <div class="position-absolute attributes top"> Type: 
+                    <br> {{ type }} </div>
+
+                <div class="position-absolute attributes bottom"> Price: {{ price }} </div>
+            </div>
+        </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+
+.top{
+    top: 0;
+    background-color: rgba(0, 0, 0, 0.650);
+}
+.attributes {
+    position: absolute;
+    width: 100%;
+    color: white;
+    padding: 1rem 0;
+    display: none;
+}
+.bottom{
+    bottom: 28%;
+    background-color: rgba(0, 0, 0, 0.650);
+}
+.card:hover .attributes{
+    display: block;
+}
+.cards-img {
+    width: 100%;
+    height: 200px;
+    aspect-ratio: 1/1;
+}
+</style>
